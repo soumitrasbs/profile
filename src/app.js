ed8504cc60ce240399f7dfb4a152d13c4dd503a7
Soumitra Bag <soumitrasbs.com>
2021-06-23 10:47:01 +0530

    Website Hosting

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app =express();
+const path = require('path');
+
+const port=process.env.PORT || 8000
+// public path 
+const static_path = path.join(__dirname,"../public");
+app.use(express.static( static_path));
+
+// routing 
+app.get("", (req,res)=>{
+    res.send("Welcome to Soumitra Bag");
+})
+
+app.get("/index", (req,res)=>{
+    res.send("Welcome to Soumitra Bag");
+})
+
+
+app.get("*", (req,res)=>{
+    res.send("404 Error page");
+})
+
+
+
+
+
+
+
+
+app.listen(port,()=>{
+    console.log(`Server start at the port number ${port}`)
+})
